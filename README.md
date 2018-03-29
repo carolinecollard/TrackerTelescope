6c5e36f541e4757101e8d1d9dae41ae69ffc0b1d
Caroline <caroline.collard@cern.ch>
2018-03-29 12:26:04 +0200

    first commit of the full framework

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # TrackerTelescope
+
+cmsrel CMSSW_9_2_13
+
+cd CMSSW_9_2_13/src/
+
+git init
+
+git clone https://github.com/carolinecollard/TrackerTelescope.git
+
+mv TrackerTelescope/RecoTracker .
+
+mv TrackerTelescope/Configuration .
+
+scramv1 b
+
+cmsenv
+
+cmsRun TrackerTelescope/test_caro/Config_reco.py
---
+++ test_caro/Config_reco.py
@@ -0,0 +1,98 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step3 --conditions 92X_dataRun2_Prompt_v11 -s RAW2DIGI,L1Reco,RECO --runUnscheduled --process reRECO --data --era Run2_2017 --eventcontent RECO --scenario cosmics --datatier RECO --customise Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2017 --no_exec -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('reRECO',eras.Run2_2017)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContentCosmics_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.MagneticField_0T_cff') 
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.ReconstructionCosmics_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring('file:step3_DIGI2RAW.root'),
+    fileNames = cms.untracked.vstring('root://sbgse1.in2p3.fr//dpm/in2p3.fr/home/cms/phedex/store/data/Commissioning2017/Cosmics/RAW/v1/000/290/129/00000/2292B0CD-F912-E711-A7F9-02163E0145B3.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step3 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('/opt/sbg/cms/ui3_data1/ccollard/test_desesp_step3_modif10_tracksP5.root'),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_Prompt_v11', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstructionCosmics)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.RECOoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.RecoTLR
+from Configuration.DataProcessing.RecoTLR import customisePostEra_Run2_2017 
+
+#call to customisation function customisePostEra_Run2_2017 imported from Configuration.DataProcessing.RecoTLR
+process = customisePostEra_Run2_2017(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
